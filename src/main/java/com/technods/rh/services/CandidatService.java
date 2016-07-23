42b5cf039953ec355182c2a1c4107177233d326e
SeyfAllahCheref <seifallahcheref@gmail.com>
2016-07-23 18:27:58 +0100

    View file for the candidat.adding candidat.

---
+++ src/main/java/com/technods/rh/services/CandidatService.java
@@ -0,0 +1,10 @@
+package com.technods.rh.services;
+
+import com.technods.rh.entities.Candidat;
+
+//import java.util.List;
+
+public interface CandidatService {
+	
+public void addCandidat(Candidat c);
+}
---
+++ src/main/java/com/technods/rh/services/impl/CandidatServiceImpl.java
@@ -0,0 +1,31 @@
+package com.technods.rh.services.impl;
+
+import com.technods.rh.entities.Employee;
+import com.technods.rh.entities.Candidat;
+import com.technods.rh.entities.repositories.CandidatRepository;
+import com.technods.rh.services.CandidatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by TECHNO on 11/05/2016.
+ */
+@Service
+public class CandidatServiceImpl implements CandidatService{
+
+
+    @Autowired
+    CandidatRepository candidatRepository;
+
+	@Override
+	public void addCandidat(Candidat c) {
+		candidatRepository.save(c);
+		
+	}
+    
+    
+
+}
